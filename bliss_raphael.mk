3f4112bca0861b9f9fcd8adc07c3931355a42343
ralf979 <ralf979@gmail.com>
2020-07-16 10:11:14 +0200

    Add ANXCamera inbuild

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -32,3 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="raphael"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/ANXCamera/config.mk)
